f33c84aebcc2979a4b08fc85e10b332da44c14bb
Dominik Ivaska <ivaskad@gmail.com>
2018-05-25 01:59:45 +0200

    Fix audioContext issue after chrome update

---
+++ src/components/Welcome.vue
@@ -0,0 +1,42 @@
+<template lang="html">
+  <div class="component welcome" v-draggable.window>
+    <div class="component-header-controll-area">
+      <div class="component-header-controll circle"></div>
+    </div>
+    <h2 class="drag-anchor">Welcome</h2>
+    <div class="section">
+      <div class="component-block">
+        <h3>Go to Vuetone</h3>
+        <div class="component-block-content">
+          <a href="/vuetone" @click.prevent="startAudioContext">Start audio context and continue to Vuetone</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Tone from 'tone'
+
+export default {
+  methods: {
+    startAudioContext: function () {
+      if (Tone.context.state !== 'running') {
+        console.log('resuming startAudioContext...')
+        Tone.context.resume()
+      }
+      this.$router.push({path: 'vuetone'})
+    }
+  },
+  beforeRouteUpdate: function (to, from, next) {
+    next()
+  }
+}
+</script>
+
+<style lang="css">
+.component.welcome {
+  left: calc(50% - 340px / 2);
+  top: calc(50% - 104px / 2);
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index'
+import Welcome from '@/components/Welcome'
 
 Vue.use(Router)
 
@@ -8,6 +9,11 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'Welcome',
+      component: Welcome
+    },
+    {
+      path: '/vuetone',
       name: 'Index',
       component: Index
     }
